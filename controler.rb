aff040033009bf9980529c309a6ad09b947266d3
GianClaudio Scarafini <g.claudio.scarafini@gmail.com>
2023-12-27 16:20:51 +0000

    test

--- controler.rb
+++ controler.rb
@@ -1,12 +1,21 @@
+require_relative 'task'
+
 class Controler
+  def initialize(view, repository)
+    @view = view
+    @repository = repository
+  end
 
   def add_task
     #1) ask user for a task
     #2) take user's input
+    title = @view.ask_user_for_a_title
 
     #3) create instance of task from inputs
+    task = Task.new(title)
 
-    #4) GIve task to the repository
+    #4) Give task to the repository
+    @repository.add(task)
 
   end
 
---
+++ view.rb
@@ -0,0 +1,12 @@
+
+class View
+  def initialize
+
+  end
+
+  def ask_user_for_a_title
+    puts "what do you want to do?"
+    puts.chomp
+
+  end
+end
